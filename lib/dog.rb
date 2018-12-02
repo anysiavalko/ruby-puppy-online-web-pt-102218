db9978955d9efd82222a891d7b49c54912f3ec09
Anysia V <anysiavalko@gmail.com>
2018-12-02 19:30:50 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Dog
+  
+  
+  @@all = [ ]
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all.each do |name|
+      puts name 
+    end
+  end 
+ 
+end
+    
